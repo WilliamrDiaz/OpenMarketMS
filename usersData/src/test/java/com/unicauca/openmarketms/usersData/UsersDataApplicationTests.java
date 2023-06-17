cad70d9f1f1461e0db3b7c8ea10f72d1b0028a5c
william <wrdiaz@unicauca.edu.co>
2023-06-17 14:18:00 -0500

    micro servicios: crudProducts, search, usersData

---
+++ usersData/src/test/java/com/unicauca/openmarketms/usersData/UsersDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicauca.openmarketms.usersData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
